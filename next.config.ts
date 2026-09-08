bf4322cae14e2663954931fca06b5789f72454d9
ayoub <ayoub@ayoubs-MacBook-Air.local>
2026-09-08 01:18:51 +0100

    Update layout and fix meta tags

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,22 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   // Avoid picking a parent-folder lockfile as the tracing root when multiple exist
   outputFileTracingRoot: __dirname,
+  // Product ZIP templates under pack/ are customer downloads, not part of this app
+  outputFileTracingExcludes: {
+    '*': ['./pack/**/*', './midlleware/**/*'],
+  },
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [
+        '**/node_modules/**',
+        '**/pack/**',
+        '**/midlleware/**',
+        '**/.git/**',
+      ],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
